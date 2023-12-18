c38d8c5870d03a44c8ae95a5a713b23632a1d05b
Kenniku Tolato <kenniku_you@ymail.com>
2023-12-18 01:43:13 +0000

    Adjust classes and add ex6.rb

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bobyy", last_name: "Lee", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Andrew", last_name: "Santinto", hourly_rate: 60)
+@store2.employees.create(first_name: "Rudy", last_name: "Jules", hourly_rate: 50)
