26f52b886a8c7783be134296481a55b340d750f3
Kenniku Tolato <kenniku_you@ymail.com>
2023-12-19 06:09:39 +0000

    Add ex7

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,7 +8,7 @@ puts "----------"
 # Burnaby (annual_revenue of 300000, carries men's and women's apparel)
 burnaby = Store.create(name: "Burnaby Store", annual_revenue: 30000, mens_apparel: true, womens_apparel: true )
 # - Richmond (annual_revenue of 1260000 carries women's apparel only)
-richmond = Store.create(name: "Richmond Store", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+richmond = Store.create(name: "Richmond Store", annual_revenue: 1260000, mens_apparel: true, womens_apparel: true)
 # - Gastown (annual_revenue of 190000 carries men's apparel only)
 gastown = Store.create(name: "Gastown Store", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+puts "Store Creation - Enter a Store Name: "
+store_name = gets.chomp
+
+
+new_store = Store.create(name: "#{store_name}", annual_revenue: nil , mens_apparel: nil , womens_apparel: nil)
+
+
+if new_store.valid?
+  puts "Store created successfully!"
+else
+  puts "Error creating store:"
+  new_store.errors.full_messages.each { |message|
+    puts "- #{message}"
+  }
+end
